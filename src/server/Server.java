d5ddb51ea8a394e014385d76a9ab89cd0608113c
alex-4reg <alex.4reg@zoho.com>
2022-06-29 10:36:40 +0400

    1-я версия решения ДЗ

--- src/server/Server.java
+++ src/server/Server.java
@@ -6,14 +6,17 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Server {
     public static void main(String[] args) {
         ArrayList<Socket> sockets = new ArrayList<>();
+        HashMap<Integer, String> clientData = new HashMap<>();
+
         try {
             ServerSocket serverSocket = new ServerSocket(9178);
             System.out.println("Сервер запущен!");
-            while (true){
+            while (true) {
                 // Создаём сокет для подключившегося клиента
                 Socket socket = serverSocket.accept();
                 sockets.add(socket);
@@ -23,15 +26,28 @@ public class Server {
                     public void run() {
                         try {
                             DataInputStream is = new DataInputStream(socket.getInputStream());
+                            DataInputStream in = new DataInputStream(socket.getInputStream());
                             // Поток вывода для socket
                             // Отправили вопрос "Как тебя зовут?"
                             // Получили ответ и запомнили
-                            while (true){
+                            while (true) {
                                 String request = is.readUTF();
                                 System.out.println(request);
-                                for (Socket socket1: sockets){
+                                for (Socket socket1 : sockets) {
                                     DataOutputStream out = new DataOutputStream(socket1.getOutputStream());
-                                    out.writeUTF("Server: "+request);
+                                    out.writeUTF("Server: " + request);
+                                    System.out.println(socket1.getPort());
+                                    //clientData.putIfAbsent(socket1.getPort()) socket1.getPort()
+                                    /**/
+                                    int clientPort = socket1.getPort();
+                                    if (!clientData.containsKey(clientPort)) {
+                                        out.writeUTF("Как тебя зовут?");
+                                        String name = in.readUTF();
+                                        clientData.put(clientPort, name);
+                                        out.writeUTF("Привет, " + name + ". Ваш порт = " + clientPort);
+                                    }
+                                    //System.out.println(socket1.getPort());
+                                    //if(userNames. != null) out.writeUTF("Как тебя зовут?");
                                 }
                             }
                         } catch (IOException e) {
@@ -41,7 +57,6 @@ public class Server {
                     }
                 });
                 thread.start();
-
             }
 
         } catch (IOException e) {
